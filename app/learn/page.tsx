26c8485d91779ada632e1d0925f418627c8d6aea
alzahya05 <alzahya05@gmail.com>
2026-05-03 15:30:12 +0800

    update

---
+++ app/learn/page.tsx
@@ -0,0 +1,80 @@
+export default function learn() {
+    return (
+        <div className="bg-white h-screen flex items-center justify-center flex items-between justify between">
+        <div className="bg-purple-300 h-72 w-96  flex flex-col justify-between rounded-4x1 text-blue-950">
+            <div className="flex gap-4">
+                <img src="/palette.svg"/>
+                <p> Design </p>
+            </div>
+            <div>
+                <h1 className="text-3xl font-bold"> Figma </h1>
+                <p> 8 YEARS AGO</p>
+            </div>
+        </div>
+        <div className="h-screen flex items-center justif-center">
+            <div className="flex gap-4 px-4">
+                <div className="flex-col justify-between">
+
+                </div>
+
+            </div>
+        </div>
+        <div className="bg-white h-screen flex items-center justify-center flex items-between justify between" >
+            <div className="bg-orange-300 h-72 w-96  flex flex-col justify-between rounded-4x1 text-blue-950">
+                <div className="flex gap-4">
+                    <img src="/palette.svg"/>
+                    <p>Codding</p>
+                </div>
+                <div>
+                    <h1 className="text-3x1 font-bold">Python</h1>   
+                    <p> 2 day ago</p>                
+            </div>
+        </div>  
+
+        <div className="h-screen flex item-center justify center">
+            <div className="flex gap-4 px-4">
+
+            </div>
+            </div> 
+
+
+            <div className="bg-white h-screen flex items-center justify-center flex items-between justify between" >
+                <div className="bg-red-300 h-72 w-96  flex flex-col justify-between rounded-4x1 text-blue-950" >
+                    <div className="flex gap-4">
+                        <img src="/palette.svg"/>
+                        <p>design</p>
+
+                    </div>
+                    <div>
+                        <h1 className="text-3x1 font-bold">skecth</h1>
+                        <p> 4 day ago</p>
+                    </div>
+
+                </div>
+
+                <div className="h-screen flex items-center justify-center">
+                    <div className="flex gap-4 px-4">
+                    </div>
+
+                    <div className="bg-white h-screen">
+                        <div className="bg-blue-200 h-70 w-100 flex flex-col justify-between p-8 rounded-4-1 text-blue-950">
+                            <div className="flex gap-4">
+                                <img src="/palette.svg"/>
+                                <p>Design</p>
+                            </div>
+                            <div>
+                                <h1 className="text-3x1 font bold">Adobe Photoshop</h1>
+                                <p> in 3 days</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    );
+}
