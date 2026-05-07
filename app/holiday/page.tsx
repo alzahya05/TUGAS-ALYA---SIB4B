ed7aa1993d5eb91fabad5405ff00ac4a6f9d4055
alzahya05 <alzahya05@gmail.com>
2026-05-07 11:57:08 +0800

    update

---
+++ app/holiday/page.tsx
@@ -0,0 +1,25 @@
+export default function holiday () {
+    return (
+        <div className="bg-white h-screen p-10 flex flex-wrap justify-center items-start gap-6">
+            <div className="bg-gray-300 h-140 w-90 flex flex-col justify-center p-8 rounded-4x1 text-blue-950">
+                <div className="flex gap-6"> 
+                    <div>
+                        <img src="/pantai1.jpg" className="rounded-4xl w-100 h-70"/> 
+                        <h1 className="text-3xl font-bold">San Francisco</h1>
+                        <div className="flex items-center justif-center"> 
+                            <p>Premium economy</p>
+                            <div className="flex">
+                                
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
---
+++ app/latihan1/page.tsx
@@ -0,0 +1,28 @@
+export default function latihan1 () {
+    return (
+        <div className="bg-purple h-screen flex items-center justify-center flex items-between justify between ">
+            <div className="bg-white-300 h-60 w-60 flex flex-col justify-between p-8 rounded-4xl text-blue-950">
+                <div className="bg-blue-200 w-60 flex itmes-center">
+
+                </div>
+                <div className="flex gap-4">
+                    <div className="flex-col text-black-950">
+                    <p>Explore</p>
+                    <div>
+                    </div>
+                    <h1 className="text-3xl fontbold">Nadine</h1>
+                    <h1 className="flex justify-between items-end"></h1>
+                        <p>iya dong</p>
+                        
+                        <div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
